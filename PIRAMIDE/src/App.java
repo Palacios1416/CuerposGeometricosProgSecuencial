a0a7e58495d0813e5d046de8fb94932d6b3060a1
Dereck Palacios <dpalaciosurbi@gmail.com>
2023-08-25 15:20:34 -0600

    Piramide comenzada

---
+++ PIRAMIDE/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        double arealateral, areatotal, volumen;
+        double pBase, aBase, apotema, apotemaLado;
+        System.out.println("");
+    }
+}
