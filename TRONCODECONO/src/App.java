7c69d819dacb4fb6be1a30f68663d6cc4b49940e
Dereck Palacios <dpalaciosurbi@gmail.com>
2023-08-25 23:05:47 -0600

    TRONCODECONO COMENZADO

---
+++ TRONCODECONO/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner (System.in);
+
+        double areaLateral, areaTotal, volumen;
+        double generatriz, aBasemayor, altura, radiomayor, radiomenor, aBasemenor;
+
+        
+
+    }
+}
